38308316c4dac9680caef1b96320cf00e6585f50
EIGordey <101456033+EIGordey@users.noreply.github.com>
2022-03-18 19:21:11 +0300

    some changes

---
+++ RickAndMortyApp/Extantions/CollectionViewControllerExtentions.swift
@@ -0,0 +1,28 @@
+//
+//  CollectionViewControllerExtentions.swift
+//  RickAndMortyApp
+//
+//  Created by Евгений Гордей on 12.03.22.
+//
+
+import UIKit
+
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    // 4
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
--- RickAndMortyApp/extantions/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  RickAndMortyApp
-//
-//  Created by Евгений Гордей on 7.03.22.
-//
-
-import Foundation
